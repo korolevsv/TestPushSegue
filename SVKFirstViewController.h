553b450e159d3a3aeea49869e71bbab5c3276afb
Sergey Korolev <korolevsv@gmail.com>
2014-08-11 17:44:49 +0400

    Test project for Push Segue force action

---
+++ SVKFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  SVKFirstViewController.h
+//  TestPushSegue
+//
+//  Created by Sergey Korolev on 11.08.14.
+//  Copyright (c) 2014 SelfEducation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVKFirstViewController : UIViewController
+
+@end
---
+++ SVKFirstViewController.m
@@ -0,0 +1,52 @@
+//
+//  SVKFirstViewController.m
+//  TestPushSegue
+//
+//  Created by Sergey Korolev on 11.08.14.
+//  Copyright (c) 2014 SelfEducation. All rights reserved.
+//
+
+#import "SVKFirstViewController.h"
+
+@interface SVKFirstViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *secondButton;
+
+@end
+
+@implementation SVKFirstViewController
+- (IBAction)secondButtonAction:(id)sender {
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
