c0ef1661a13e559e5bb399b29096187d331b5ab1
Sergey Korolev <korolevsv@gmail.com>
2014-08-11 17:34:45 +0400

    Initial Commit

---
+++ TestPushSegue/SVKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SVKAppDelegate.h
+//  TestPushSegue
+//
+//  Created by Sergey Korolev on 11.08.14.
+//  Copyright (c) 2014 SelfEducation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestPushSegue/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestPushSegue
+//
+//  Created by Sergey Korolev on 11.08.14.
+//  Copyright (c) 2014 SelfEducation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SVKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SVKAppDelegate class]));
+    }
+}
